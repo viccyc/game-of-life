73510c681e3d6a82f019e6eef63a17316fe10c5b
Viccy Grace <viccy.c.grace@gmail.com>
2019-12-08 20:29:44 -0700

    created skeleton grid layout tests

--- src/testjs/grid-layout.test.js
+++ src/testjs/grid-layout.test.js
@@ -1,2 +1,17 @@
 const grid = require('../js/grid-layout');
 
+test('initializes grids', () => {
+  // expect(grid.initializeGrids()).toBe('');
+});
+
+test('creates a 10 x 10 table', () => {
+  // expect(grid.createTable()).toBe('');
+});
+
+test('changes color of cell when clicked', () => {
+  // expect(grid.cellClickHandler()).toBe('');
+});
+
+test('creates 2 buttons', () => {
+  // expect(grid.setupControlButtons()).toBe('');
+});
